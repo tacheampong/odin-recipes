f3dab82ff18ec47acd1adf2f7369803f3434236b
tacheampong <th.acheampong@gmail.com>
2024-07-17 23:05:07 -0400

    Rename and ChangePage

--- recipes/home.js
+++ recipes/home.js
@@ -1,8 +1,34 @@
-const name = document.getElementById("food-name");
+const foodName = document.getElementById("food-name");
+const toPage = document.getElementById("forward");
 
+
+if(toPage){
+
+    toPage.addEventListener("click", ChangePage);
+    toPage.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior:"smooth"
+    })
+}
 
 function Home() {
     return window.location.href = "../index.html";
 
 }
-name.addEventListener("click", Home);
+
+function ChangePage() {
+
+    if (window.location.href === "http://" + window.location.host.concat("/odin-recipes/recipes/cheesecake.html")) {
+        return window.location.href = "http://" + window.location.host.concat("/odin-recipes/recipes/fried-chicken.html");
+
+    }
+    if (window.location.href === "http://" + window.location.host.concat("/odin-recipes/recipes/fried-chicken.html")) {
+        return window.location.href = "http://" + window.location.host.concat("/odin-recipes/recipes/taco-dip.html");
+
+
+    }
+    if (window.location.href === "http://" + window.location.host.concat("/odin-recipes/recipes/taco-dip.html")) {
+        return window.location.href = "http://" + window.location.host.concat("/odin-recipes/recipes/cheesecake.html");
+
+    }
+}
+
